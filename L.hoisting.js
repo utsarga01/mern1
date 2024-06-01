d9274846bb2f21835cf322c4c4be3e8e6b96f831
utsarga01 <regmiutsarga7@gmail.com>
2024-06-01 23:22:36 +0545

    hoisting

--- L.hoisting.js
+++ L.hoisting.js
@@ -4,3 +4,22 @@
 
 
 // arrowFunction() const arrowFunction = () => { console. log("This is an arrow function.") } Unlike regular functions, attempting to call an arrow function before it's declared will result in a reference error, 
+// function printHello() {
+//     console.log("hello")
+//   }
+  
+//   printHello()
+//   hello
+printHello()
+// hello
+
+// printDillion()
+// ReferenceError: printDillion is not defined
+
+// function printHello() {
+//   console.log('hello')
+
+//   function printDillion() {
+//     console.log("dillion")
+//   }
+// }
