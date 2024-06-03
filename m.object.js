160849c55d20698a593756f4e7ff1cb3ffc40f87
utsarga01 <regmiutsarga7@gmail.com>
2024-06-03 22:02:49 +0545

    objects

---
+++ m.object.js
@@ -0,0 +1,37 @@
+// ? objects
+// objects are variable too. But objects contain many values.
+// ? creating an object
+// const car = {type:"Hyundai",model:"i20",color:"white"};
+// const person ={firstName:"Utsarga",lastName:"Regmi",age:21};
+
+
+// ? creating an empty object and adding properties to it.
+//creating object
+// const person = {};
+//Adding properties
+// person.firstName = "Utsarga";
+// person.lastName = "Regmi";
+// person.age = 21;
+/*
+accessing object properties
+objectName.propertyName
+or
+objectName["propertyName"]
+*/
+//  const person = {
+
+//  firstName :"Utsarga",
+//  lastName : "Regmi",
+//  age : 21,
+//  fullName:function concat(){
+//     return this.firstName + " " + this.lastName;
+//  }
+//  };
+
+ 
+
+
+
+
+
+
