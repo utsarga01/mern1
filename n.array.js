9514bae5eab6157b91988e1f304d49c54d57f156
utsarga01 <regmiutsarga7@gmail.com>
2024-06-05 21:45:13 +0545

    array

---
+++ n.array.js
@@ -0,0 +1,30 @@
+//creating an array
+// const array_name=[item1,item2,...];
+const cars = ["Toyota", "BMW", "Skoda"];
+//converting array to string
+
+//javascript method toString() converts an array to a string of (comma separated) array values.
+
+const fruits = ["Banana", "orange", "Apple", "Mango"];
+// console.log(fruits.toString());
+
+//gives Banana,orange,Apple,Mango
+//Accessing 1st array element
+// let fruit = fruits[0];
+
+// console.log(fruit);
+
+// Accessing the lasst array element
+
+// let fruit = fruits[fruits.length-1];
+
+//looping Array Elements
+
+// let fLen = fruits.length;
+// let text = "<ul>";
+// for (let i = 0; i < fLen; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+
+// text += "</ul>";
+// console.log(text);
