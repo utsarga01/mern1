b56b56cade4fb01e08a56e30b2e21c62ea8f7281
utsarga01 <regmiutsarga7@gmail.com>
2024-06-06 22:45:09 +0545

    more on arrays

--- n.array.js
+++ n.array.js
@@ -1,11 +1,11 @@
 //creating an array
 // const array_name=[item1,item2,...];
-const cars = ["Toyota", "BMW", "Skoda"];
+//const cars = ["Toyota", "BMW", "Skoda"];
 //converting array to string
 
 //javascript method toString() converts an array to a string of (comma separated) array values.
 
-const fruits = ["Banana", "orange", "Apple", "Mango"];
+//const fruits = ["Banana", "orange", "Apple", "Mango"];
 // console.log(fruits.toString());
 
 //gives Banana,orange,Apple,Mango
@@ -28,3 +28,13 @@ const fruits = ["Banana", "orange", "Apple", "Mango"];
 
 // text += "</ul>";
 // console.log(text);
+// ? Array pop
+const fruits = ["Apple", "Banana", "Mango", "Litchi"];
+// fruits.pop(); //removes last element Litchi and only Apple, Banana and mango remains
+// console.log(fruits);
+// fruits.push("Guava");//adds Guava
+// console.log(fruits);
+fruits.shift(); //removes first elements..
+//console.log(fruits);
+delete fruits[0];//leaves the hole in the array..
+console.log(fruits);
