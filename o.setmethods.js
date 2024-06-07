365967827643c225f4cd4e86b51096be69ee2fcb
utsarga01 <regmiutsarga7@gmail.com>
2024-06-07 21:11:30 +0545

    setmethods

---
+++ o.setmethods.js
@@ -0,0 +1,9 @@
+// Create a Set
+// const letters = new Set(["a","b","c"]);
+//has() method
+// The has() method returns true if a specified value exists in a set.
+// Create a Set
+// const letters = new Set(["a","b","c"]);
+
+// Does the Set contain "d"?
+// answer = letters.has("d");
