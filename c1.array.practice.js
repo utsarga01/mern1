c2ac27c2ab39c24e0a210da4a277a0a3be111a4c
utsarga01 <regmiutsarga7@gmail.com>
2024-06-14 10:07:13 +0545

    mapfilter

--- c1.array.practice.js
+++ c1.array.practice.js
@@ -44,7 +44,9 @@ const laptopList = [
 // find "Zenbook 15" laptop details
 // ?find sum of prices of all laptops
 // const newLaptopList = laptopList.map((item,index,array) => {
+
 //     const discountedPrice = item.price - (6/100) *item.price;
+
 //     return{
 //         ...item,
 //         price:discountedPrice,
@@ -55,7 +57,51 @@ const laptopList = [
 
 // ?5 get all the laptops whose price is below $1050
 
-const newLaptopList = laptopList.filter((item,index,array) => item.price<1050);
-console.log(newLaptopList);
+// const newLaptopList = laptopList.filter(
+//   (item, index, array) => item.price < 1050
+// );
+// console.log(newLaptopList);
 
 // ?7
+
+// const sumOfLaptops = laptopList.reduce((sum, item) => {
+//   sum = sum + item.price;
+//   return sum;
+// }, 0);
+
+// console.log(sumOfLaptops);
+
+// ? increase price of Dell laptops by 10%
+// const increasedPrice = laptopList.map((item) => {
+//   if (item.brand === "Dell") {
+//     const newPrice = item.price + (1 / 10) * item.price;
+
+//     return {
+//       ...item,
+//       price: newPrice,
+
+//   };
+// }
+
+//   return {...item};
+// });
+// console.log(increasedPrice);
+
+// find all the lenovo laptops
+// const searchLaptop = laptopList.filter((item, index, array) => {
+//   return item.brand === "Lenovo";
+// });
+// console.log(searchLaptop);
+
+// ? update name of "IdeaPad" to "Idea Pad Ultra"
+const updateName = laptopList.map((item) => {
+  if (item.name === "Idea Pad") {
+    return {
+      ...item,
+      name: "Idea Pad Ultra",
+    };
+  }
+  return { ...item };
+});
+
+console.log(updateName);
