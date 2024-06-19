18b7562f844ba4a8f021344113eb22c736d9d68a
utsarga01 <regmiutsarga7@gmail.com>
2024-06-19 21:51:18 +0545

    more on filter and reduce

---
+++ arraymore.filter.js
@@ -0,0 +1,18 @@
+// const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter((word) => word.length > 6);
+
+// console.log(result);
+// Expected output: Array ["exuberant", "destruction", "present"]
+
+// const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue,
+// );
+
+// console.log(sumWithInitial);
+// Expected output: 10
