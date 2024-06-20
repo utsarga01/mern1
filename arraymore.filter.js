1dc15c6997643f212f9d33d86f26d4dcbff339c1
utsarga01 <regmiutsarga7@gmail.com>
2024-06-20 07:35:16 +0545

    ...

--- arraymore.filter.js
+++ arraymore.filter.js
@@ -16,3 +16,11 @@
 
 // console.log(sumWithInitial);
 // Expected output: 10
+// const arr = new Array(3);
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] = new Array(4).fill(1); // Creating an array of size 4 and filled of 1
+// }
+// arr[0][0] = 10;
+// console.log(arr[0][0]); // 10
+// console.log(arr[1][0]); // 1
+// console.log(arr[2][0]); // 1
