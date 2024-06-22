0bf2668a59a8a7cbbe94a4b680dd0f55bec3858e
utsarga01 <regmiutsarga7@gmail.com>
2024-06-22 20:34:40 +0545

    Splice

--- array8.filter.js
+++ array8.filter.js
@@ -24,3 +24,13 @@
 // console.log(arr[0][0]); // 10
 // console.log(arr[1][0]); // 1
 // console.log(arr[2][0]); // 1
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 0, 'Feb');
+// Inserts at index 1
+// console.log(months);
+ // Expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+// months.splice(4, 1, 'May');
+ // Replaces 1 element at index 4
+// console.log(months);
+ // Expected output: Array ["Jan", "Feb", "March", "April", "May"]
--- d.operator.js
+++ d.operator.js
@@ -7,44 +7,45 @@
 // arithmetic operator
 // ? +, -,*,/,**,%
 
-console.log(Math.ceil(25 / 2)); //also Math.floor can be used
+// console.log(Math.ceil(25 / 2)); 
+//also Math.floor can be used
 
 //assignment operator
 
 // ? =,+=,-=,*=,/=,**=,%=
-let x = 5;
-x = x + 2;
-console.log(x);
+// let x = 5;
+// x = x + 2;
+// console.log(x);
 
 // ?comparison operator
 // ? == , ===, != ,!==,<,<=,>,>=
 
-let c = 3;
-let b = 5;
+// let c = 3;
+// let b = 5;
 // ? == => weak comparison,checks only value
 // ? === => strict comparison, value  + datatype
-console.log(c == b);
-console.log(c === b);
+// console.log(c == b);
+// console.log(c === b);
 
-let u = "Utsarga";
-let v = "utsarga";
-console.log(u.toLowerCase() === v);
+// let u = "Utsarga";
+// let v = "utsarga";
+// console.log(u.toLowerCase() === v);
 
 // ? logical operator
 // AND (&&)
 // OR(||)
 // NOT(!)
-console.log(true && false);
-console.log(true || false);
-console.log(!true);
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!true);
 
-let cond1 = 3 > 5; //false
-let cond2 = "nepal" === "Nepal";
-console.log(cond1 && cond2);
+// let cond1 = 3 > 5; //false
+// let cond2 = "nepal" === "Nepal";
+// console.log(cond1 && cond2);
 
-let i=50;
-let l=6;
-console.log(Math.ceil(50/6));
+// let i=50;
+// let l=6;
+// console.log(Math.ceil(50/6));
 
 
 
