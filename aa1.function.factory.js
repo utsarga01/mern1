2129a2ebd4c79d7daa676617ee3d7a88476b35c3
utsarga01 <regmiutsarga7@gmail.com>
2024-07-19 22:38:35 +0545

    funcfactory

---
+++ aa1.function.factory.js
@@ -0,0 +1,11 @@
+// ?........... Function factory............. 
+// ?A function factory is a function that creates and returns other functions. It's a powerful concept in JavaScript (and other programming languages) that allows for the creation of customized functions on-the-fly.
+// function createGreetingFunction(greeting) {
+//     return function(name) {
+//       console.log(`${greeting}, ${name}!`);
+//     };
+//   }
+  
+//   const sayHello = createGreetingFunction("Hello");
+//   const sayHi = createGreetingFunction("Hi");
+//   const sayGreetings = createGreetingFunction("Greetings");
